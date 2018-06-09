9d618399234ef1a58b18f43604690475dc624855
Sanhueza <31940962+Sanhueza@users.noreply.github.com>
2018-06-08 21:20:09 -0400

    Solo seleccionar (no funcional)
    Cambié muchas cosas... lo único que si funciona es que son seleccionables las listas, pero no funciona el método al que lleva.

--- bin/Spotify.cs
+++ bin/Spotify.cs
@@ -9,20 +9,22 @@ using System.Web.UI.HtmlControls;
 
 public class Spotify: Page{
 
-  protected List<cancion> mus = new List<cancion>();
-  protected List<cancion> lirep = new List<cancion>();
-  protected HtmlGenericControl limus;
-  protected cancion cncn=new cancion();
+  protected List<cancion> mus = new List<cancion>(); //lista de música
+  protected List<cancion> lirep = new List<cancion>();//lista de reproducción
+  protected HtmlGenericControl limus;//lista donde se ve la música en la vista
 
   protected void Page_Load(){
-    if(IsPostBack){
-      miBase();
-    }
+    miBase();
+  }
+
+  void agregar(object sender, EventArgs e){
+    Response.Write("<p>"+sender.ToString()+"</p>");
+    Console.Write("asdfghjgfdsasdfgh");
   }
 
   public void miBase(){
     try{
-      string @base="Server=127.0.0.1;Database=prufi;Uid=root;Pwd='mariadb';sslmode=none";
+      string @base="Server=127.0.0.1;Database=prufi;Uid=root;Pwd='';sslmode=none";
       MySqlConnection conexion = new MySqlConnection(@base);
 
       conexion.Open();
@@ -35,15 +37,19 @@ public class Spotify: Page{
       //miComando.ExecuteNonQuery();
 
       while(registros.Read()){
+        //Esto guarda los datos de las canciones en la lista de musica
         cancion a = new cancion(registros["mus_id"].ToString(),registros["mus_nombre"].ToString(),registros["mus_url"].ToString());
         mus.Add(a);
+
+        //Esta parte hace la lista
         HtmlGenericControl li = new HtmlGenericControl("li");
-        li.Attributes.Add("class", registros["mus_id"].ToString());
-        li.InnerText = registros["mus_nombre"].ToString();
+        li.Attributes.Add("id", registros["mus_id"].ToString());
+        li.InnerHtml = "<asp:LinkButton runat='server' OnClick='agregar' id='"+registros["mus_id"].ToString()+"'>"+registros["mus_nombre"].ToString()+"</asp:LinkButton>";
         limus.Controls.Add(li);
 
         //Response.Write("<h1>"+registros["mus_nombre"].ToString()+"</h1>");
       }
+
     }catch(MySqlException ex){
       Response.Write(ex.ToString());
     }
--- vistaSpotify.aspx
+++ vistaSpotify.aspx
@@ -7,18 +7,18 @@
 	</head>
 	<body>
 		<form action=? runat="server" method="post">
-			<audio controls>
-  				<source src="musica\01-I.mp3" type="audio/ogg">
-				Tu navegador no puede reproducir esta música.
-			</audio>
+			
 			<div>
 				<h3>Musica</h3>
-				<input type="submit" value="Cargar" />
 				<ul runat="server" id="limus"> </ul>
 			</div>
 			<div>
+				<audio controls>
+  					<source src="musica\01-I.mp3" type="audio/ogg">
+					Tu navegador no puede reproducir esta música.
+				</audio>
 				<h3>Lista de reproduccion</h3>
-				<ul runat="server" id="lirep"> </ul>
+				<ul runat="server" id="lirep"></ul>
 			</div>
 		</form>
 	</body>
