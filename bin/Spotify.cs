ea8b9d08533905243d55f1e86a99263976d57768
Sanhueza <31940962+Sanhueza@users.noreply.github.com>
2018-06-11 23:20:37 -0400

    Listo
    Ya está. Traté de comentar lo más que pude para que se pueda entender mejor.

--- bin/Spotify.cs
+++ bin/Spotify.cs
@@ -14,44 +14,26 @@ public class Spotify: Page{
   protected HtmlGenericControl limus;//lista donde se ve la música para escoger en la vista
   protected HtmlGenericControl lirep;//lista donde se ve la música a reproducir en la vista
   protected HtmlGenericControl reproductor;//este es el reproductor de audio
+  protected HtmlGenericControl lilirep;//es para todos los li de la lista de reproducción
+  protected HtmlGenericControl lilimus;//es para todos los li de la lista de musica
 
   protected void Page_Init(object sender, EventArgs e){
-    miBase();//carga la lista de música
+    if(!IsPostBack){//limpia la lista dereproducción cuando la página está recien cargada
+      conectar("limpiar","no");
+    }
+    conectar("paramus","no");//llena mus desde la base de datos
+    conectar("pararep","no");//llena rep desde la base de datos
+    cargarReproductor();//carga el reproductor
   }
 
-  //Solo llega agregar una canción porque cuando se vuelve a seleccionar algo carga toda la página de nuevo, por lo demás funciona
+  //agrega la canción seleccionada a la lista de reproducción
   public void agregar(object sender, EventArgs e){
     try{
       LinkButton idcan=sender as LinkButton;//idcan es la canción seleccionada
       foreach (cancion canci in mus){//recorremos la lista para buscar la canción
         if(canci.id==idcan.ID){
-          rep.Add(canci);//se agrega a la lista reproducción
-          cambiarCancion(canci);//se cambia la canción en el reproductor
-          // Ahora se procede a insertar la cancion en la playlist
-          string datab = "Server=127.0.0.1;Database=prufi;Uid=root;Pwd='';sslmode=none";
-          MySqlConnection dba = new MySqlConnection(datab);
-          dba.Open();
-          string validar = "select * from music_play where musica_id = "+canci.id+" and playlist_id = 1";
-          MySqlCommand comando = new MySqlCommand(validar, dba); 
-          MySqlDataReader readerValidar = comando.ExecuteReader();  
-          bool noExiste = true;
-          // Se busca si la cancion ya se encuentra registrada en la playlist
-          while(readerValidar.Read()){
-            noExiste = false;
-          }
-          readerValidar.Close();
-          // En caso de que no se encuentre registrada se agrega a la playlist
-          if(noExiste){
-            string insertar = "insert into music_play (musica_id, playlist_id) values ("+canci.id+",1)"; 
-            comando = new MySqlCommand(insertar, dba); 
-            MySqlDataReader readerInsert = comando.ExecuteReader();  
-            readerInsert.Close();
-            cargarReproduccion();//se agrega a la lista visual de reproducción
-            Page.ClientScript.RegisterStartupScript(GetType(),"errorAdd","addCancion()",true); //Ejecuta la funcion js addCancion() en el archivo vistaSpotify.aspx
-          }else{
-            Page.ClientScript.RegisterStartupScript(GetType(),"errorAdd","errorAdd()",true); // Ejecuta la funcion js errorAdd() en el archivo vistaSpotify.aspx
-          }
-          dba.Close();
+          conectar("agrega",canci.id);//se agrega a la base de datos
+          cargarReproductor();//carga el reproductor con la canción
         }
       }
     }catch(MySqlException ex){
@@ -59,82 +41,145 @@ public class Spotify: Page{
     }
   }
 
-  public void cambiarCancion(cancion tocando){//cambia la canción que está puesta en el reproductor
-    HtmlGenericControl source=new HtmlGenericControl("source");
-    source.Attributes.Add("src",tocando.url);
-    reproductor.Controls.Add(source);
+  public void cargarReproductor(){//carga las canciones de la lista en el reporductor
+    reproductor.Controls.Clear();//limpia el reproductor
+    foreach(cancion tocar in rep){
+      HtmlGenericControl source=new HtmlGenericControl("source");
+      source.Attributes.Add("src",tocar.url);
+      reproductor.Controls.Add(source);
+    }
   }
 
-  //Esta debería quitar la cancion seleccionada de la lista de reproducción, pero no llega a ejecutarse, no entiendo porqué 
+  public void tocarSiguiente(object sender, EventArgs e){//elimina la primera canción de la lista para que se toque la siguiente
+    //esto es para mantener la integridad de la list, está explicado en el método de abajo
+    List<cancion> listAux = new List<cancion>(); 
+    listAux.Add(rep[0]);
+    rep.Remove(listAux[0]);
+    conectar("quita",listAux[0].id);
+  }
+    
+  //quita la canción seleccionada de la lista de reproducción 
   public void quitar(object sender,EventArgs e){
-    Response.Write("Holaaaaaaaaaaaaaaaaaaa");
     LinkButton idcan=sender as LinkButton;//idcan es la canción seleccionada
-    foreach (cancion canci in rep){//recorremos la lista para buscar la canción
-      if(canci.id==idcan.ID){
-        rep.Remove(canci);//se quita la canción de la lista
+    string siId=idcan.ID.Trim(new Char[]{'c'});//Es para sacarle la c del principio al id
 
-        //Esto no debería ir, pero lo dejo porque hay que tener en cuenta en qué quedará el reproductor:
-        cambiarCancion(canci);
+    //esto es para mantener la integridad. En List no se puede solo borrar
+    List<cancion> listAux = new List<cancion>(); 
+    foreach(cancion canci in rep){//recorremos la lista para buscar la canción
+      if(canci.id==siId){
+        listAux.Add(canci);//se guarda la que se va a borrar en este list auxiliar
+        break;
       }
     }
-    cargarReproduccion();//Vuelve a cargar para que cargue la lista sin la canción que se acaba de sacar
+    foreach (cancion item in listAux){
+        rep.Remove(item);//ahora si permite removerlo
+        conectar("quita",item.id);//lo quitamos también de la base de datos
+    }
   }
   
-  //como la página carga cada de nuevo cada vez despues de clickear solo guarda una canción por vez
-  public void cargarReproduccion(){
+  //carga la lista de reporducción en la vista
+  public void cargarRep(){
+    lirep.Controls.Clear();//limpiamos para que no se dupliquen
     foreach (cancion canci in rep){//recorremos la lista para cargar la lista de reproducción entera
       HtmlGenericControl li = new HtmlGenericControl("li");
       LinkButton tocar = new LinkButton();
       tocar.Text = canci.nombre;
-      tocar.ID = canci.id;
+      tocar.ID = "c"+canci.id;
       tocar.Click += new System.EventHandler(quitar);
       li.Controls.Add(tocar);
       lirep.Controls.Add(li);
     }
+    cargarReproductor();
   }
 
-  public void miBase(){
-    try{
+  public void cargarMus(){//carga la lista para escoger la música
+    foreach(cancion canci in mus){
+      HtmlGenericControl li = new HtmlGenericControl("li");
+      LinkButton botonesA = new LinkButton();
+      botonesA.Text = canci.nombre;
+      botonesA.ID = canci.id;
+      botonesA.Click += new System.EventHandler(agregar);
+      li.Controls.Add(botonesA);
+      limus.Controls.Add(li);
+    }
+  }
+
+//esta función concentra todas las actividades relacionadas con la base de datos
+  public void conectar(string op, string no){
+    try{//inicair conexión
       string @base="Server=127.0.0.1;Database=prufi;Uid=root;Pwd='';sslmode=none";
       MySqlConnection conexion = new MySqlConnection(@base);
-
       conexion.Open();
+      MySqlCommand comando;
+      MySqlDataReader datos;
+      string miSql;
 
-      string miSql = "obtener_musica";
+    switch (op){
 
-      MySqlCommand miComando = new MySqlCommand(miSql,conexion);
-      miComando.CommandType = CommandType.StoredProcedure;
-      MySqlDataReader registros = miComando.ExecuteReader();
-      //miComando.ExecuteNonQuery();
+        case "paramus"://obtener música
+          miSql = "obtener_musica";
+          comando = new MySqlCommand(miSql,conexion);
+          comando.CommandType = CommandType.StoredProcedure;
+          datos = comando.ExecuteReader();
+          while(datos.Read()){//Esto guarda los datos de las canciones en la lista de musica (mus)
+            cancion ca = new cancion(datos["mus_id"].ToString(),datos["mus_nombre"].ToString(),datos["mus_url"].ToString());
+            mus.Add(ca);
+          }
+          datos.Close();
+          cargarMus();
+          break;
 
-      while(registros.Read()){
-        //Esto guarda los datos de las canciones en la lista de musica
-        cancion ca = new cancion(registros["mus_id"].ToString(),registros["mus_nombre"].ToString(),registros["mus_url"].ToString());
-        mus.Add(ca);
+        case "pararep"://obtener la lista de reproducción de la base de datos
+          rep.Clear();
+          miSql = "select mus_id, mus_nombre, mus_url from musica left join music_play on mus_id = musica_id where playlist_id = 1 group by mus_id";
+          comando = new MySqlCommand(miSql,conexion);
+          datos = comando.ExecuteReader();
+          while(datos.Read()){//carga las canciones en la listade reproducción (rep)
+            cancion c = new cancion(datos["mus_id"].ToString(),datos["mus_nombre"].ToString(),datos["mus_url"].ToString());
+            rep.Add(c);
+          }
+          datos.Close();
+          cargarRep();
+          break;
 
-        //Esta parte hace la lista
-        HtmlGenericControl li = new HtmlGenericControl("li");
-        LinkButton botonesA = new LinkButton();
-        botonesA.Text = registros["mus_nombre"].ToString();
-        botonesA.ID = registros["mus_id"].ToString();
-        botonesA.Click += new System.EventHandler(agregar);
-        li.Controls.Add(botonesA);
-        limus.Controls.Add(li);
-      }
-      registros.Close();
-      miSql = "select mus_id, mus_nombre, mus_url from musica left join music_play on mus_id = musica_id where playlist_id = 1 group by mus_id";
-      MySqlCommand comando = new MySqlCommand(miSql,conexion);
-      MySqlDataReader playlist = comando.ExecuteReader();
-      while(playlist.Read()){
-        //Esto busca todas la canciones de la lista principal
-        cancion c = new cancion(playlist["mus_id"].ToString(),playlist["mus_nombre"].ToString(),playlist["mus_url"].ToString());
-        HtmlGenericControl li = new HtmlGenericControl("li");
-        LinkButton tocar = new LinkButton();
-        tocar.Text = c.nombre;
-        tocar.ID = "cancion"+c.id;
-        tocar.Click += new System.EventHandler(quitar);
-        li.Controls.Add(tocar);
-        lirep.Controls.Add(li);
+        case "agrega"://agrega una canción a la playlist
+          miSql = "select * from music_play where musica_id = "+no+" and playlist_id = 1";//no es el id
+          comando = new MySqlCommand(miSql, conexion); 
+          datos = comando.ExecuteReader();  
+          bool noExiste = true;
+          // Se busca si la cancion ya se encuentra registrada en la playlist
+          while(datos.Read()){
+            noExiste = false;
+          }
+          datos.Close();
+          // En caso de que no se encuentre registrada se agrega a la playlist
+          if(noExiste){
+            miSql = "insert into music_play (musica_id, playlist_id) values ("+no+",1)"; 
+            comando = new MySqlCommand(miSql, conexion); 
+            datos = comando.ExecuteReader();  
+            datos.Close();
+            Page.ClientScript.RegisterStartupScript(GetType(),"errorAdd","addCancion()",true); //Ejecuta la funcion js addCancion() en el archivo vistaSpotify.aspx
+          }else{
+            Page.ClientScript.RegisterStartupScript(GetType(),"errorAdd","errorAdd()",true); // Ejecuta la funcion js errorAdd() en el archivo vistaSpotify.aspx
+          }
+          conectar("pararep","no");
+          break;
+
+        case "quita"://quita la canción de la playlist
+          miSql = "DELETE FROM `prufi`.`music_play` WHERE  `musica_id`="+no+" AND `playlist_id`=1";
+          comando = new MySqlCommand(miSql,conexion);
+          comando.ExecuteNonQuery();
+          cargarRep();
+          break;
+
+        case "limpiar"://limpia la base de datos al cargar la página, podemos usarla o no
+          miSql ="DELETE FROM `prufi`.`music_play` WHERE `playlist_id`=1";
+          comando = new MySqlCommand(miSql,conexion);
+          comando.ExecuteNonQuery();
+          break;
+
+        default:
+          break;
       }
       conexion.Close();
     }catch(MySqlException ex){
--- vistaSpotify.aspx
+++ vistaSpotify.aspx
@@ -5,6 +5,17 @@
 		<title>Spotify pero no</title>
 		<meta charset="utf-8">
 		<script language="javascript" type="text/javascript">
+			function funcion_inicial(){
+				var audio = document.getElementById("reproductor");
+				//ejecuta el botón cuanto termina una canción para pasar a la siguiente  desde el code Behind
+				audio.addEventListener("ended", function() {
+					botonOcultoEjecutar();
+				});
+			}
+			function botonOcultoEjecutar(){
+				var boton = document.getElementById('<%=btnEjecutar.ClientID%>');
+				boton.click(); 
+			}
 			function errorAdd(){
 				alert("Esta cancion ya se encuentra en la playlist");
 			}
@@ -13,7 +24,7 @@
 			}
 		</script>
 	</head>
-	<body>
+	<body onload="funcion_inicial()">
 		<form action=? runat="server" method="post">
 			
 			<div>
@@ -23,10 +34,12 @@
 			</div>
 			<div>
 				<h3>Lista de reproduccion</h3>
-				<ul runat="server" id="lirep"></ul>
-				<audio runat="server" id="reproductor" controls>
-					Tu navegador no puede reproducir esta música.
+				<audio runat="server" id="reproductor" controls autoplay>
 				</audio>
+				<ul runat="server" id="lirep"></ul>
+			</div>
+			<div id="botones"><!--Este es un botón oculto para poder manejar la música desde el code Behind-->
+				<asp:Button id="btnEjecutar" runat="server" Text="Get" Style="display: none" OnClick="tocarSiguiente"/>
 			</div>
 		</form>
 	</body>
